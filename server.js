0a2be4d0528e6668d86fff4b2a99e75b976ae55a
Bradford Foxworth-Hill <bradford.foxworthhill@gmail.com>
2024-06-06 22:59:47 -0400

    changed stuff

--- server.js
+++ server.js
@@ -5,4 +5,5 @@ app.get('/', (req, res) => {
     res.send(["Brad", "Eric", "John"])
 })
 
-app.listen(4000, () => console.log('Server running on port 4000'))
+const port = process.env.PORT || 4000
+app.listen(port, () => console.log(`Server running on port ${port}`))
